b88e371929654f0fc482455bed086a1b570e39c7
Michael-Ngochi <ngoochimike@gmail.com>
2025-06-24 14:50:36 +0300

    added init file for models

---
+++ server/models/__init__.py
@@ -0,0 +1,6 @@
+from .user import User
+from .property import Property
+from .property_media import PropertyMedia
+from .inquiry import Inquiry
+from .viewing_request import ViewingRequest
+from .application import Application
